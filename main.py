877d4eb5cea1fd429343936c94b461cea3aec289
burkhon-n <158993562+burkhon-n@users.noreply.github.com>
2024-09-16 13:41:00 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, Response, request
+import requests
+from config import *
+from telebot.types import Update
+from bot import *
+
+app = Flask(__name__)
+
+@app.route("/api", methods=["POST"])
+def api():
+    data = request.json
+    update = Update.de_json(data)
+    if update.message:
+        if update.message.text:
+            if update.message.text.startswith("/"):
+                command_handler(update.message)
+            else:
+                message_handler(update.message)
+        else:
+            message_handler(update.message)
+    elif update.callback_query:
+        inline_handler(update.callback_query)
+    return Response("OK", status=200)
+
+@app.route("/setwebhook")
+def setwebhook():
+    try:
+        requests.get(f"https://api.telegram.org/bot{TOKEN}/deleteWebhook")
+        requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={URL}/api")
+        return Response("Done", status=200)
+    except Exception as e:
+        return Response(str(e), status=500)
+
+app.run(host="localhost", port=8888)
