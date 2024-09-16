21ceb10fc2fa7a128f80e2e60af6c20fe284ce4f
burkhon-n <158993562+burkhon-n@users.noreply.github.com>
2024-09-16 15:05:56 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 from telebot import TeleBot
 from telebot.types import Message, CallbackQuery
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-from config import TOKEN, ADMIN_ID, CHANNEL_ID, messages, json, get_blocked_users
+from config import TOKEN, ADMIN_ID, CHANNEL, messages, json
 import re
+
+def get_blocked_users():
+    with open("blocked_users.json", "r") as file:
+        return json.load(file)
 
 def block_user(user_id):
     blocked_users = get_blocked_users()
@@ -35,6 +39,7 @@ def command_handler(message: Message):
         bot.send_message(message.chat.id, "<b>Invalid Command</b> ❌", parse_mode="HTML")
 
 def message_handler(message: Message):
+    print(message.json)
     blocked_users = get_blocked_users()
     if str(message.chat.id) in blocked_users:
         approvement_keyboard = InlineKeyboardMarkup()
@@ -56,7 +61,7 @@ def message_handler(message: Message):
             )
             bot.copy_message(ADMIN_ID, message.chat.id, message.message_id, reply_markup=approvement_keyboard)
         else:
-            mess = bot.copy_message(CHANNEL_ID, message.chat.id, message.message_id)
+            mess = bot.copy_message(CHANNEL, message.chat.id, message.message_id)
             bot.send_message(message.chat.id, "<b>Message Sent</b> ✅", parse_mode="HTML")
             admin_buttons = InlineKeyboardMarkup()
             admin_buttons.row(
@@ -64,16 +69,22 @@ def message_handler(message: Message):
                 InlineKeyboardButton("🛑 Block User", callback_data=f"block {message.chat.id}"),
                 InlineKeyboardButton("🗑 Delete Message", callback_data=f"delete {mess.message_id}")
             )
+            admin_buttons.row(
+                InlineKeyboardButton("🔗 Go to the post", url=f"t.me/{CHANNEL[1:]}/{mess.message_id}")
+            )
             bot.copy_message(ADMIN_ID, message.chat.id, message.message_id, reply_markup=admin_buttons)
     
     elif message.photo or message.video or message.audio or message.voice or message.document:
-        mess = bot.copy_message(CHANNEL_ID, message.chat.id, message.message_id)
+        mess = bot.copy_message(CHANNEL, message.chat.id, message.message_id)
         bot.send_message(message.chat.id, "<b>Message Sent</b> ✅", parse_mode="HTML")
         admin_buttons = InlineKeyboardMarkup()
         admin_buttons.row(
             InlineKeyboardButton("👌 Ignore", callback_data=f"ignore"),
             InlineKeyboardButton("🛑 Block User", callback_data=f"block {message.chat.id}"),
             InlineKeyboardButton("🗑 Delete Message", callback_data=f"delete {mess.message_id}")
+        )
+        admin_buttons.row(
+            InlineKeyboardButton("🔗 Go to the post", url=f"t.me/{CHANNEL[1:]}/{mess.message_id}")
         )
         bot.copy_message(ADMIN_ID, message.chat.id, message.message_id, reply_markup=admin_buttons)
     else:
@@ -82,7 +93,7 @@ def message_handler(message: Message):
 def inline_handler(call: CallbackQuery):
     data = call.data.split()
     if data[0] == "approve":
-        bot.copy_message(CHANNEL_ID, data[2], int(data[1]))
+        bot.copy_message(CHANNEL, data[2], int(data[1]))
         bot.send_message(data[2], "<b>Message Sent</b> ✅", parse_mode="HTML")
         bot.answer_callback_query(call.id, "Message Approved")
         bot.delete_message(ADMIN_ID, call.message.message_id)
@@ -104,6 +115,6 @@ def inline_handler(call: CallbackQuery):
         unblock_user(data[1])
         bot.send_message(data[1], "<b>You have been unblocked</b> ✅", parse_mode="HTML")
     elif data[0] == "delete":
-        bot.delete_message(CHANNEL_ID, int(data[1]))
+        bot.delete_message(CHANNEL, int(data[1]))
         bot.answer_callback_query(call.id, "Message Deleted")
         bot.delete_message(ADMIN_ID, call.message.message_id)
